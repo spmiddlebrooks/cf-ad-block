cbc6cdbc3283c5037ec5d4b79d227a08c0238d36
spmiddlebrooks <spmiddlebrooks@gmail.com>
2023-04-27 08:57:12 -0400

    Delete variables.tf

--- variables.tf
+++
@@ -1,4 +0,0 @@
-variable "cloudflare_account_id" {
-  type        = string
-  description = "Cloudflare account id, set it in 'terraform.tfstate' file or 'TF_VAR_cloudflare_account_id' environment var"
-}
