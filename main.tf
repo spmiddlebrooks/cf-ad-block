b8bc1c1075d432ccff6652d65609373a36eb5c4a
spmiddlebrooks <spmiddlebrooks@gmail.com>
2023-04-27 08:02:49 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -13,5 +13,5 @@ terraform {
 
 variable "cloudflare_account_id" {
  type        = string
- default     = {{ $secrets.CLOUDFLARE_ACCOUNT_ID }}
+ default     = "${{ secrets.CLOUDFLARE_ACCOUNT_ID }}"
 }
