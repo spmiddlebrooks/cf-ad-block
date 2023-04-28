dcedf962fc0479a985701e5415941c99590f43cd
spmiddlebrooks <spmiddlebrooks@gmail.com>
2023-04-28 08:41:04 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -11,10 +11,13 @@ terraform {
   required_version = ">= 1.1.0"
 }
 
-variable "cloudflare_api_token" {
- type        = string
+variable "zone_id" {
+  default = "14c60605fcfc8d89331d1c039fb92e5d"
+}
+
+variable "account_id" {
+  default = "7e9918092d616256ce61ad41e0319dba"
 }
 
 provider "cloudflare" {
-  api_token = var.cloudflare_api_token
 }
